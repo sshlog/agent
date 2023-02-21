da512ec26f1de92c2e819fefa6b2cb0f4f12432a
Matt Hill <matt@openkilt.com>
2023-02-20 22:14:58 -0600

    Made script for packaging deb

--- package_installers.sh
+++ package_installers.sh
@@ -8,16 +8,30 @@ TAR_FILE_NAME=sshbouncer
 rm -Rf $VOL_SHARE_DIR
 mkdir -p $VOL_SHARE_DIR
 
+# Copy the signing key
+cp signing_keys/private.pgp ${VOL_SHARE_DIR}
+
 git-archive-all --force-submodules --prefix=sshbouncer/ ${VOL_SHARE_DIR}/sshbouncer.tar.gz
 cd $VOL_SHARE_DIR
 tar xvfz sshbouncer.tar.gz
 
+
+HOST_USER_ID=$(id -u)
+HOST_GROUP_ID=$(id -g)
+
+# This script executes inside the docker container in order to build the deb files and place them in the volume mount
 echo """#!/bin/bash
 
-cd ${VOL_SHARE_DIR}
+gpg --import ${VOL_SHARE_DIR}/private.pgp
+
+cd ${VOL_SHARE_DIR}/sshbouncer/
+pip3 install -r daemon/requirements.txt 
 debuild -b
+chown ${HOST_USER_ID}:${HOST_GROUP_ID} ${VOL_SHARE_DIR}/* -R
 """ > ${VOL_SHARE_DIR}/sshbouncer/pack.sh
 chmod +x ${VOL_SHARE_DIR}/sshbouncer/pack.sh
 
 
 docker run --rm -v ${VOL_SHARE_DIR}:${VOL_SHARE_DIR} -it openkilt/builder:ubuntu2004 ${VOL_SHARE_DIR}/sshbouncer/pack.sh
+
+echo "Deb packages available in ${VOL_SHARE_DIR}"
