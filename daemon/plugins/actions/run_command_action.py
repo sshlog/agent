ee892b70250e4572265af7dfe1d5593f303b283f
Matt Hill <matt@openkilt.com>
2023-02-22 13:51:25 -0600

    Logging on command action when exit code != 0

--- daemon/plugins/actions/run_command_action.py
+++ daemon/plugins/actions/run_command_action.py
@@ -19,5 +19,8 @@ class run_command_action(ActionPlugin):
             args_list.append(self._insert_event_data(event_data, arg))
         args_list.extend(self.args)
 
-        self.logger.debug(f"Executing command f{args_list}")
-        subprocess.call(args_list)        self.logger.info(f"{self.name} Command action triggered on {event_data['event_type']} executing {args_list}")
+        self.logger.info(f"{self.name} Command action triggered on {event_data['event_type']} executing {args_list}")
+        exit_code = subprocess.call(args_list, timeout=self.timeout)
+
+        if exit_code != 0:
+            self.logger.info(f"Command {args_list} returned failure exit code {exit_code}")
